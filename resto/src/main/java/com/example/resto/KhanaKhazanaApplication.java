8776e0e4aa051e6fddb4f70d6d82a2ab30b74755
vaibhav-bavkar <bavkarvaibhav18@gmail.com>
2025-08-08 10:17:04 +0530

    First Commit

--- resto/src/main/java/com/example/resto/KhanaKhazanaApplication.java
+++ resto/src/main/java/com/example/resto/KhanaKhazanaApplication.java
@@ -8,6 +8,7 @@ public class KhanaKhazanaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KhanaKhazanaApplication.class, args);
+		System.out.println("Hello Wolrd");
 	}
 
 }
